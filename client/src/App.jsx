997015f0435ba86bc67e29d225fab0d7b467ec23
Jerry Britto <jbritto2004@gmail.com>
2024-06-26 22:55:35 +0530

    done with navbar

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Header/Navbar";
+import Login from "./components/Login/Login.jsx"
+
+export default function App() {
+  return (
+    // <Navbar/>
+    <Login />
+  )
+}
+
+
